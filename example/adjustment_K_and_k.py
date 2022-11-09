835d0e149d65085d89d98cab2f747ce1074b0ba8
baugetfa <fabrice.bauget@supagro.fr>
2022-11-09 16:58:38 +0100

    clean up

--- example/adjustment_K_and_k.py
+++ example/adjustment_K_and_k.py
@@ -18,11 +18,13 @@ Usage:
 
 Inputs:
     - yaml file given in command line argument
-    - data/arabido_data.csv: see beginning of the main part, csv file containing the cut and flow data with the following columns:
-            - arch: sample name that must be contained in the 'input_file' of the yaml file
-            - Jbase: flux of the full root
-            - J1, ..., Jn: columns containing the flux values of 1st cut, 2d cut, etc.
-            - lcut1, ...., lcutn: columns containing the maximum length to the base after each cut, 1st cut, 2d cut, etc.
+    - data/cnf_data.csv: may be changed see begining of main, csv file containing data of cut and flow data of with
+            the following columns:
+            - arch: sample name, the string must be contained in the 'input_file' name given in the yaml file
+            - dP_Mpa: column with the working cut and flow pressure (in relative to the base) if constant, may be empty see below
+            - J0, J1, ..., Jn: columns that start with 'J' containing the flux values, 1st the for the full root, then 1st cut, 2d cut, etc.
+            - lcut1, ...., lcutn: columns starting with 'lcut' containing the maximum length to the base after each cut, 1st cut, 2d cut, etc. (not the for full root)
+            - dP0, dP1,.., dPn: column starting with 'dP' containing the working pressure (in relative to the base) of each steps (if not constant): full root, 1st cut, 2d cut, etc.
 
 Remark: at this stage 2022-07-29, this script is used for arabidopsis and for experiment done at a constant working pressure
         given in the yaml file, unlike adjustment_K_k_Js_Ps.py where the script has been used with CnF experiment where
@@ -32,16 +34,16 @@ Outputs:
     - console:
         - CnF: plant name, max length (m), k (10-8 m/s/MPa), total length (m), surface (m2), Jv (microL/s)
     - matplotlib:
-        - 1 plot:
+        - 2 plots:
             - Jv(l) cnf): Jv exp dot, Jv sim line
+            - K(x): K 1st, K adjusted (displayed if adjustment asked)
+
     - outputfile (csv):
         - column names: 'plant', 'cut length (m)', 'primary_length (m)', 'k (10-8 m/s/MPa)', '_length (m)',
                         'surface (m2)', 'Jv (uL/s)', 'Jexp (uL/s)'
         - if Flag_Optim add the following: 'x', 'K 1st', 'K optimized'
                                             the initial and adjusted K(x)
 
-example:
-    - see notebook boursiac2022 Figure 2-B: '%run adjustment_K_and_k.py parameters_fig-2-B.yml -op'
 """
 
 import numpy as np
@@ -50,6 +52,7 @@ import argparse
 import time
 import copy
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from scipy import optimize
 
@@ -90,19 +93,47 @@ if Flag_Optim is None: Flag_Optim = False
 # if Flag_verbose is None: Flag_verbose = False
 parameter.read_file(filename)
 
-def hydro_calculation(g, axfold = 1., radfold = 1., axial_data = None, k_radial = None):
+def plot_architecture():
+    from openalea.plantgl.algo.view import interactive_view, view
+    from hydroroot.display import mtg_scene
+    from ipywidgets import interact, fixed
+
+    def my_view(cut: str = 'tot', prop: str = 'j', imgsize: tuple = (800, 800), perspective: bool = True, zoom: float = 1,
+                azimuth: float = 0, elevation: float = 0, line_width = 1.0):
+
+        list_prop = {'radial flux' : 'j', 'xylem pressure' : 'psi_in', 'axial K' : 'K', 'axial flux' : 'J_out'}
+        
+        g = g_cut[cut].copy()
+        
+        keys = list(g.property('radius').keys())
+        radius=np.array(list(g.property('radius').values()))
+        new_radius = radius * line_width
+        g.properties()['radius'] = dict(list(zip(keys, new_radius)))
+
+        s = mtg_scene(g, prop_cmap = list_prop[prop], has_radius = True)
+        view(scene = s, imgsize = imgsize, perspective = perspective, zoom = zoom, azimuth = azimuth, elevation = elevation)
+
+    _list = ['tot']
+    for i in cut_n_flow_length:
+        _list.append(str(i))
+
+    interact(my_view, cut = _list, prop = ['radial flux', 'xylem pressure', 'axial K', 'axial flux'], imgsize = fixed((800, 800)),
+             perspective = False, zoom = (0.01, 1), azimuth = (-180, 180), elevation = (-90, 90), line_width = (1, 5))
+
+def hydro_calculation(g, axfold = 1., radfold = 1., axial_data = None, k_radial = None, psi_base = 0.1, psi_e = 0.1):
     if axial_data is None: axial_data = parameter.hydro['axial_conductance_data']
     if k_radial is None: k_radial = parameter.hydro['k0']
     # compute axial & radial
     Kexp_axial_data = conductance.axial(axial_data, axfold)
     k_radial_data = conductance.radial(k_radial, axial_data, radfold)
+    # k_radial_data = conductance.radial_step(k_radial,3.0,x_step = 0.01, dx = parameter.archi['segment_length'], scale = radfold)
 
     # compute local jv and psi, global Jv, Keq
     g, Keq, Jv = hydroroot_flow(g, segment_length = parameter.archi['segment_length'],
                                        k0 = k_radial,
-                                       Jv = parameter.exp['Jv'],
-                                       psi_e = parameter.exp['psi_e'],
-                                       psi_base = parameter.exp['psi_base'],
+                                       Jv = _Jv[0],
+                                       psi_e = psi_e,
+                                       psi_base = psi_base,
                                        axial_conductivity_data = Kexp_axial_data,
                                        radial_conductivity_data = k_radial_data)
 
@@ -120,12 +151,10 @@ def fun1(x):
     axfold = x[0]
     radfold = x[-1]
 
-    psi_e = parameter.exp['psi_e']
-    psi_base = parameter.exp['psi_base']
-
-    g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'], radfold = radfold, axfold = axfold)
-    F = (Jv - parameter.exp['Jv']) ** 2.0
-    count = 0
+    g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'], radfold = radfold, axfold = axfold, psi_base = psi_base,
+                                              psi_e = psi_base + DP_cnf[0])
+    F = (Jv - _Jv[0]) ** 2.0
+    count = 1
     for cut_length in cut_n_flow_length:
         _g = g_cut[str(cut_length)].copy()
 
@@ -137,10 +166,9 @@ def fun1(x):
             v = g_cut['tot'].parent(i)
             _g.property('k')[v] = _g.property('K')[v]
 
-        _g = flux.flux(_g, Jv = _Jv[count], psi_e = psi_e, psi_base = psi_base,
+        _g = flux.flux(_g, Jv = _Jv[count], psi_e = psi_base + DP_cnf[count], psi_base = psi_base,
                        invert_model = True, cut_and_flow = True)
-        keq = _g.property('Keq')[1]
-        Jv = keq * (psi_e - psi_base)
+        Jv = _g.property('J_out')[1]
         F += (Jv - _Jv[count]) ** 2.0
 
         count += 1
@@ -156,18 +184,16 @@ def fun2(x):
     :param x: the array of adjusted parameters
     :return: F the sum((Jv - Jv_exp) ** 2.0)
     """
-    k0 = parameter.hydro['k0']
+    # k0 = parameter.hydro['k0']
 
     axial_data = copy.deepcopy(parameter.hydro['axial_conductance_data'])
     axial_data[1] = list(x)
 
-    psi_e = parameter.exp['psi_e']
-    psi_base = parameter.exp['psi_base']
-
-    g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'], k_radial = k0 ,axial_data = axial_data)
-    F = (Jv - parameter.exp['Jv'])**2.0 
+    g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'], k_radial = k0 ,axial_data = axial_data, psi_base = psi_base,
+                                              psi_e = psi_base + DP_cnf[0])
+    F = (Jv - _Jv[0])**2.0
     
-    count = 0
+    count = 1
     for cut_length in cut_n_flow_length:
         _g = g_cut[str(cut_length)].copy()
 
@@ -179,14 +205,13 @@ def fun2(x):
             v = g_cut['tot'].parent(i)
             _g.property('k')[v] = _g.property('K')[v]
 
-        _g = flux.flux(_g, Jv = _Jv[count], psi_e = psi_e, psi_base = psi_base,
+        _g = flux.flux(_g, Jv = _Jv[count], psi_e = psi_base + DP_cnf[count], psi_base = psi_base,
                        invert_model = True, cut_and_flow = True)
-        keq = _g.property('Keq')[1]
-        Jv = keq * (psi_e - psi_base)
+        Jv = _g.property('J_out')[1]
         F += (Jv - _Jv[count])**2.0 
 
         count += 1
-        
+
     return F
 
 def fun3(x):
@@ -198,15 +223,15 @@ def fun3(x):
     :param x: the array of adjusted parameters
     :return: F the sum((Jv - Jv_exp) ** 2.0)
     """
-    count = 0
+
     g_cut['tot']  = conductance.compute_k(g_cut['tot'] , k0 = x[0])
     
-    g_cut['tot'] = flux.flux(g_cut['tot'], Jv =  parameter.exp['Jv'], psi_e = psi_e, psi_base = psi_base,
+    g_cut['tot'] = flux.flux(g_cut['tot'], Jv =  _Jv[0], psi_e = psi_base + DP_cnf[0], psi_base = psi_base,
                        invert_model = True)
-    keq = g_cut['tot'].property('Keq')[1]
-    Jv = keq * (psi_e - psi_base)
-    F = (Jv - parameter.exp['Jv']) ** 2.0
+    Jv = g_cut['tot'].property('J_out')[1]
+    F = (Jv - _Jv[0]) ** 2.0
 
+    count = 1
     for cut_length in cut_n_flow_length:
         _g = g_cut[str(cut_length)].copy()
 
@@ -218,10 +243,9 @@ def fun3(x):
             v = g_cut['tot'].parent(i)
             _g.property('k')[v] = _g.property('K')[v]
 
-        _g = flux.flux(_g, Jv = _Jv[count], psi_e = psi_e, psi_base = psi_base,
+        _g = flux.flux(_g, Jv = _Jv[count], psi_e = psi_base + DP_cnf[count], psi_base = psi_base,
                        invert_model = True, cut_and_flow = True)
-        keq = _g.property('Keq')[1]
-        Jv = keq * (psi_e - psi_base)
+        Jv = _g.property('J_out')[1]
         F += (Jv - _Jv[count]) ** 2.0
 
         count += 1
@@ -231,14 +255,14 @@ def fun3(x):
 if __name__ == '__main__':
 
 
-    dK_constraint = -3.e-2 # dK/dx >= dK_constraint
+    dK_constraint = -3.e-2 # dK/dx >= dK_constraint # arabidopsis paper Boursiac2022
     _tol = 1.0e-9
     
     filename = []
     for f in parameter.archi['input_file']:
         filename = filename + (glob.glob(parameter.archi['input_dir'] + f))
 
-    fn = 'data/arabido_data.csv'
+    fn = 'data/cnf_data.csv'
     df_exp = pd.read_csv(fn, sep = ',', keep_default_na = True)
 
     # predict the number of simulation run
@@ -250,7 +274,7 @@ if __name__ == '__main__':
     psi_e = parameter.exp['psi_e']
     psi_base = parameter.exp['psi_base']
 
-    columns = ['plant', 'cut length (m)', 'primary_length (m)', 'k (10-8 m/s/MPa)', '_length (m)', 'surface (m2)', 'Jv (uL/s)', 'Jexp (uL/s)']
+    columns = ['plant', 'cut length (m)', 'primary_length (m)', 'k (10-9 m/s/MPa)', '_length (m)', 'surface (m2)', 'Jv (uL/s)', 'Jexp (uL/s)']
 
     results = {}
     for key in columns:
@@ -262,12 +286,20 @@ if __name__ == '__main__':
 
         # read the data measurements from data base
         for key in df_exp['arch']:
-            if key in index.lower():
+            if str(key).lower() in index.lower():
                 _list = df_exp[df_exp.arch == key].filter(regex = '^J').dropna(axis = 1).values.tolist()
-                parameter.exp['Jv'] = _list[0][0] # basal output flux full root (uncut)
-                _Jv = _list[0][1:]                # basal output flux cut root
+                # parameter.exp['Jv'] = _list[0][0] # basal output flux full root (uncut)
+                # _Jv = _list[0][1:]                # basal output flux cut root
+                _Jv = _list[0]
                 _list = df_exp[df_exp.arch == key].filter(regex = '^lcut').dropna(axis = 1).values.tolist()
                 cut_n_flow_length = _list[0]      # cut lengthes
+                _list = df_exp[df_exp.arch == key].filter(regex = '^dP').dropna(axis = 1).values.tolist()
+                # the pressure difference is usually constant but sometimes, due to flow meter saturation, it may change
+                # in that case a list of values is given
+                if len(_list[0]) != 0:
+                    DP_cnf = _list[0]
+                    if len(DP_cnf) < len(cut_n_flow_length)+1: # if constant we create the list with the constant value
+                        for i in range(1, len(cut_n_flow_length) + 1): DP_cnf.append(_list[0][0])
 
         axfold = parameter.output['axfold'][0]
         radfold = parameter.output['radfold'][0]
@@ -275,7 +307,7 @@ if __name__ == '__main__':
         g_cut['tot'], primary_length, _length, surface, seed = root_builder(df = df, segment_length = parameter.archi['segment_length'],
             order_decrease_factor = parameter.archi['order_decrease_factor'], ref_radius = parameter.archi['ref_radius'])
 
-        g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'])
+        g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'], psi_base = psi_base, psi_e = psi_base + DP_cnf[0])
 
         ###############################################################
         #### WARNING : the mtg property 'position' must stay unchanged
@@ -315,7 +347,7 @@ if __name__ == '__main__':
         
         ## update the conductivities according to the first adjustment
         axial_data = list(conductance.axial(parameter.hydro['axial_conductance_data'], axfold))
-        parameter.hydro['k0'] = parameter.hydro['k0'] *radfold
+        k0 = parameter.hydro['k0'] *radfold
         
         ###############################################################################################
         ## 2d adjustment: 
@@ -353,10 +385,10 @@ if __name__ == '__main__':
         A[i, i] = 1.
         lb[i] = a * (l[i]-l[i-1])
 
-        k0 = parameter.hydro['k0']
+        # k0 = parameter.hydro['k0']
         k0_old = k0                        # count = 0
 
-        F_old = (Jv - parameter.exp['Jv'])**2.0
+        F_old = (Jv - _Jv[0])**2.0
 
         eps = 1e-9
         F = 1.
@@ -366,7 +398,7 @@ if __name__ == '__main__':
             k0_old2 = k0 + 10
         while abs(k0-k0_old2) > 1.0e-1:
             k0_old2 = k0
-            parameter.hydro['k0'] = k0
+            # parameter.hydro['k0'] = k0
 
             ## -1 axial data adjusted
             #########################
@@ -388,26 +420,28 @@ if __name__ == '__main__':
             k0 = resk0.x[0]
         
 
-        parameter.hydro['k0'] = k0
+        # parameter.hydro['k0'] = k0
 
     primary_length = g_cut['tot'].property('position')[1]
-    g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'], k_radial = k0 ,axial_data = axial_data)
+
+    g_cut['tot'], Keq, Jv = hydro_calculation(g_cut['tot'], k_radial = k0 ,axial_data = axial_data, psi_base = psi_base,
+                                              psi_e = psi_base + DP_cnf[0])
 
     results['plant'].append(index)
     results['primary_length (m)'].append(primary_length)
     results['cut length (m)'].append(0.0)
-    results['k (10-8 m/s/MPa)'].append(k0*0.1) #uL/s/MPa/m2 -> 10-8 m/s/MPa
+    results['k (10-9 m/s/MPa)'].append(k0)
     results['_length (m)'].append(_length)
     results['surface (m2)'].append(surface)
     results['Jv (uL/s)'].append(Jv)
-    results['Jexp (uL/s)'].append(parameter.exp['Jv'])
+    results['Jexp (uL/s)'].append(_Jv[0])
     
-    print(index, primary_length, k0*0.1, _length, surface, Jv)
+    print(index, primary_length, k0, _length, surface, Jv)
 
     ######################################
     ## Simulations with Kx and k adjusted
     ######################################
-    count = 0
+    count = 1
     for cut_length in cut_n_flow_length:
         _g = g_cut[str(cut_length)].copy()
 
@@ -419,7 +453,7 @@ if __name__ == '__main__':
             v = g_cut['tot'].parent(i)
             _g.property('k')[v] = _g.property('K')[v]
 
-        _g = flux.flux(_g, psi_e = psi_e, psi_base = psi_base, invert_model = True)
+        _g = flux.flux(_g, psi_e = psi_base + DP_cnf[count], psi_base = psi_base, invert_model = True)
 
         Jv = _g.property('J_out')[1]
         _g, surface = radius.compute_surface(_g)
@@ -429,20 +463,20 @@ if __name__ == '__main__':
         results['plant'].append(index)
         results['primary_length (m)'].append(primary_length)
         results['cut length (m)'].append(g_cut['tot'].property('position')[1] - primary_length)
-        results['k (10-8 m/s/MPa)'].append(k0*0.1) #uL/s/MPa/m2 -> 10-8 m/s/MPa
+        results['k (10-9 m/s/MPa)'].append(k0)
         results['_length (m)'].append(_length)
         results['surface (m2)'].append(surface)
         results['Jv (uL/s)'].append(Jv)
         results['Jexp (uL/s)'].append(_Jv[count])
         count += 1
-        print(index, primary_length, k0*0.1, _length, surface, Jv)
+        print(index, primary_length, k0, _length, surface, Jv)
         
 
     dresults = pd.DataFrame(results, columns = columns)
 
     ax = dresults.plot.scatter('cut length (m)', 'Jexp (uL/s)', c = 'black')
     dresults.plot.line('cut length (m)', 'Jv (uL/s)', c = 'purple', ax = ax)
-    # plt.show()
+
 
     if Flag_Optim:
         optim_results  = {}
@@ -453,8 +487,15 @@ if __name__ == '__main__':
 
         doptim = pd.DataFrame(optim_results, columns = ['x', 'K 1st', 'K optimized'])
         df = pd.concat([dresults, doptim], axis = 1)
+
+        ax_K = doptim.plot.line('x', 'K 1st', c = 'black')
+        doptim.plot.line('x', 'K optimized', c = 'purple', ax = ax_K)
     else:
         df = dresults
 
+    plt.show()
+
+    # plot_architecture()
+    
     if output is not None: df.to_csv(output, index = False)
     print('running time is ', time.time() - start_time)
